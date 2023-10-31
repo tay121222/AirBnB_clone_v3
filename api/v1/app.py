625e18e7f6adfc1470d2d523f7ec48da49a066b0
Enoch Taylor <tay121222@gmail.com>
2023-10-31 16:02:21 +0000

    HTTP access control (CORS) - updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
